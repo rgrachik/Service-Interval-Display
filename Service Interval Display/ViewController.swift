f2be92e0c618b654d80ca334a4c119ed2d1d49c8
rgrachik <112074005+rgrachik@users.noreply.github.com>
2023-01-24 22:40:57 +0300

    Initial Commit

---
+++ Service Interval Display/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Service Interval Display
+//
+//  Created by Роман Грачик on 24.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
