556d9efa403e1523eed800d3c14ad32ba76b40ee
rgrachik <112074005+rgrachik@users.noreply.github.com>
2023-01-24 23:20:44 +0300

    add storyboard

---
+++ Service Interval Display/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  Service Interval Display
+//
+//  Created by Роман Грачик on 24.01.2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
