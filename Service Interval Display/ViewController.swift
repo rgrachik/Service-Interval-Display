251742e45424e04151532b32a232d48061b9ed43
rgrachik <112074005+rgrachik@users.noreply.github.com>
2023-02-01 23:04:43 +0300

    Add Update Mileage function
    Added method and progress indicator. Added a pop-up window for entering updated mileage data.

--- Service Interval Display/ViewController.swift
+++ Service Interval Display/ViewController.swift
@@ -19,6 +19,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var nextMileageLabel: UILabel!
     
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    var updatedMileage = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,11 +30,40 @@ class ViewController: UIViewController {
         dateOfChangeLabel.text = UserDefaults.standard.string(forKey: "dateOfChange")
         currentMileageLabel.text = UserDefaults.standard.string(forKey: "mileage")
         nextMileageLabel.text = UserDefaults.standard.string(forKey: "nextMileageLabel")
+        
+        
     }
    
     
     @IBAction func changeOilButton() {
     }
+    // MARK: update mileage button
+    
+    @IBAction func updateMileageButton() {
+        
+        let alertController = UIAlertController(title: "Обновить пробег", message: "Укажите текущий пробег", preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Сохранить", style: .default) { action in
+            let textField = alertController.textFields?.first
+            if let upd = textField?.text {
+                if let intUpd = Int(upd) {
+                    self.updatedMileage = intUpd
+                }
+                
+            }
+        }
+        
+        alertController.addTextField()
+        
+        let cancelAction = UIAlertAction(title: "Отмена", style: .destructive)
+        
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true)
+    }
+    
+    
     
     @IBAction func UnwindAction(unwindSegue: UIStoryboardSegue) {
         guard unwindSegue.identifier == "backToFirst" else {return}
@@ -46,5 +78,3 @@ class ViewController: UIViewController {
         }
     }
     
-
-
